8a9a892c234dd9c143c3f30c498e028ef453f20f
silago nevermind <silago.nevermind@gmail.com>
2019-05-29 15:45:15 +0300

    fix connection termination issues

--- lib/proxy.go
+++ lib/proxy.go
@@ -44,32 +44,32 @@ func backend(c *Config, r *http.Request) (*Service, *string, error) {
 	return nil, nil, errors.New("{\"error\":\"route not found\"}")
 }
 
-func tryFallback(c *Config, r *http.Request) (string, string, bool) {
-	if c.Version != "" && c.FallbackRule != "" {
-		return c.FallbackRule, r.URL.Path, true
-	}
-	return "", "", false
-}
+//func tryFallback(c *Config, r *http.Request) (string, string, bool) {
+//	if c.Version != "" && c.FallbackRule != "" {
+//		return c.FallbackRule, r.URL.Path, true
+//	}
+//	return "", "", false
+//}
 
 func New(c *Config, middlewares map[string]func(*http.Request, func(*http.Request) (*http.Response, error)) (*http.Response, error)) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		service, query, route_error := backend(c, req)
-		if route_error == nil {
+		service, query, routeError := backend(c, req)
+		if routeError == nil {
 			switch proto := service.Protocol; proto {
 			case "ws":
 				if err := wsProxy(w, req, service, query, middlewares); err!=nil {
                     log.Println("[error][websocket]:", err.Error())
-                    w.Write([]byte(err.Error()))
+					_, _ = w.Write([]byte(err.Error()))
 				}
 			default:
 				if err := httpProxy(w, req, service, query, middlewares); err!=nil {
-					log.Println("[error][proxy]: ", err.Error())
-                    w.Write([]byte(err.Error()))
+					log.Println("[error][proxy]: ", err.Error(), service.Service, query)
+					_, _ = w.Write([]byte(err.Error()))
 				}
 			}
 		} else {
-			w.Write([]byte("{\"error\":\"page not found\"}"))
-			//tryFallback(c, req)
+			log.Println("route error", req.Host, req.URL, routeError.Error())
+			_, _ = w.Write([]byte("{\"error\":\"page not found\"}"))
 		}
 	}
 }
