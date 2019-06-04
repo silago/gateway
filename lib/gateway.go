c77390f8f2dd7705e64e2a40d7d4f5d2aa3e74d8
silago nevermind <silago.nevermind@gmail.com>
2019-06-04 10:39:50 +0300

    code refactoring

---
+++ lib/gateway.go
@@ -0,0 +1,193 @@
+package lib
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+)
+
+type Gateway struct {
+	config *Config
+	middleware map[string]PluginInterface
+	configPath string
+}
+
+func (gw *Gateway) checkIsConfigReady() {
+	if gw.config == nil {
+		panic("could not run gateway without gw.config")
+	}
+}
+
+func (gw *Gateway) StartReloadOnSignal(sig syscall.Signal) {
+	sigChan :=make(chan os.Signal, 1)
+	signal.Notify(sigChan, sig)
+	go func() {
+		for sig:=range sigChan {
+			log.Printf("got {%s}  signal. reloading config ", sig)
+			if newConfig, err := LoadConfig(gw.configPath); err == nil {
+				*gw.config=*newConfig
+			} else {
+				log.Printf(err.Error())
+				os.Exit(1)
+			}
+		}
+	}()
+	return
+}
+
+
+//func (gw *Gateway) Start() {
+//	gw.checkIsConfigReady()
+//	http.HandleFunc("/", gw.GetHandler())
+//}
+
+
+func (gw *Gateway) loadMiddleware() {
+	gw.checkIsConfigReady()
+	for pluginName, pluginPath := range gw.config.Middleware {
+		plugin, err := LoadPlugin(pluginPath)
+		if err == nil {
+			gw.middleware[pluginName] = plugin.Init()
+		} else {
+			fmt.Println("cant load plugin", pluginPath, err.Error())
+		}
+	}
+}
+
+func (gw *Gateway) startTcpPortForwarding() {
+	for host, target := range gw.config.PortForward {
+		listener, err := net.Listen("tcp", host)
+		defer listener.Close()
+		if err != nil {
+			log.Println(err.Error())
+		}
+
+		go func() {
+			for {
+				if conn, err := listener.Accept(); err!=nil {
+					log.Println(err.Error())
+					continue
+				} else if client, err := net.Dial("tcp", target); err!=nil {
+					log.Println(err.Error())
+					conn.Close()
+					continue
+				} else {
+					go func() {
+						for {
+							inputBuffer := make([]byte, 256)
+							if n, e := client.Read(inputBuffer); e != nil {
+								_ = conn.Close()
+								return
+							} else {
+								message := string(inputBuffer[:n])
+								_, _ = conn.Write([]byte(message))
+							}
+						}
+					}()
+
+					go func() {
+						for {
+							inputBuffer := make([]byte, 256)
+							if n, e := conn.Read(inputBuffer); e != nil {
+								_ = conn.Close()
+								return
+							} else {
+								message := string(inputBuffer[:n])
+								_, _ = client.Write([]byte(message))
+							}
+						}
+					}()
+				}
+			}
+		}()
+	}
+}
+
+
+func InitGateway(configPath string) *Gateway {
+	var gw = &Gateway{configPath:configPath}
+	if config, err:= LoadConfig(configPath); err!=nil {
+		panic(err)
+	} else {
+		gw.config = config
+	}
+	return gw
+}
+
+func (gw  *Gateway) GetHandler()  http.HandlerFunc { //(w http.ResponseWriter, req *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+		service, query, route_error := gw.getTargetService(req)
+		if route_error == nil {
+			switch proto := service.Protocol; proto {
+			case "ws":
+				if err :=  WebsocketProxyHandler(w, req, service, query, gw.middleware); err!=nil {
+					log.Println("[error][websocket]:", err.Error())
+					w.Write([]byte(err.Error()))
+				}
+			default:
+				if err := HttpProxyHandler(w, req, service, query, gw.middleware); err!=nil {
+					log.Println("[error][proxy]: ", err.Error())
+					w.Write([]byte(err.Error()))
+				}
+			}
+		} else {
+			w.Write([]byte("{\"error\":\"page not found\"}"))
+		}
+	}
+}
+
+//func New(c *Config, middlewares map[string]func(*http.Request, func(*http.Request) (*http.Response, error)) (*http.Response, error)) http.HandlerFunc {
+//	return func(w http.ResponseWriter, req *http.Request) {
+//		service, query, route_error := getTargetService(c, req)
+//		if route_error == nil {
+//			switch proto := service.Protocol; proto {
+//			case "ws":
+//				if err := wsProxy(w, req, service, query, middlewares); err!=nil {
+//					log.Println("[error][websocket]:", err.Error())
+//					w.Write([]byte(err.Error()))
+//				}
+//			default:
+//				if err := httpProxy(w, req, service, query, middlewares); err!=nil {
+//					log.Println("[error][proxy]: ", err.Error())
+//					w.Write([]byte(err.Error()))
+//				}
+//			}
+//		} else {
+//			w.Write([]byte("{\"error\":\"page not found\"}"))
+//		}
+//	}
+//}
+
+
+// Find service matches by the url pattern
+// returns url, service, error
+func (gw *Gateway) getTargetService(r *http.Request) (*Service, *string, error) {
+	var c = gw.config
+	var (
+		route string
+		query string
+	)
+	ps := strings.Split(r.URL.Path, "/")
+	for rule, service := range c.Rules {
+		route = "/"
+		for index, part := range ps {
+			if part == "" {
+				continue
+			}
+			route += part + "/"
+
+			if strings.Index(route, rule) == 0 {
+				query = strings.Join(ps[index+1:], "/")
+				return &service, &query, nil
+			}
+		}
+	}
+	return nil, nil, errors.New("{\"error\":\"route not found\"}")
+}
+
---
+++ lib/http_proxy.go
@@ -0,0 +1,133 @@
+package lib
+
+import (
+	"bytes"
+	"crypto/tls"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptrace"
+	"time"
+)
+
+const criticalResponseTime float64 = 0.4
+func GetDefaultHandler(service *Service, query *string) func (req *http.Request) (*http.Response, error) {
+	return func(req *http.Request) (response *http.Response, e error) {
+		body, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		req.Body = ioutil.NopCloser(bytes.NewReader(body))
+		url := fmt.Sprintf("%s://%s/%s", service.Scheme, service.Service, *query)
+		proxyReq, err := http.NewRequest(req.Method, url, bytes.NewReader(body))
+		if err!=nil {
+			return nil, err
+		}
+		proxyReq.Header = make(http.Header)
+		proxyReq.URL.RawQuery = req.URL.RawQuery
+
+		for h, val := range req.Header {
+			proxyReq.Header[h] = val
+		}
+
+		//httpClient := http.Client{}
+
+		//resp, err := httpClient.Do(proxyReq)
+
+		var start, connect, dns, tlsHandshake time.Time
+		var logs map[string]string = make(map[string]string)
+
+		trace := &httptrace.ClientTrace{
+			DNSStart: func(dsi httptrace.DNSStartInfo) { dns = time.Now() },
+			DNSDone: func(ddi httptrace.DNSDoneInfo) {
+				logs["dnsdone"] = fmt.Sprintf("DNS Done: %v", time.Since(dns).Seconds())
+			},
+
+			TLSHandshakeStart: func() { tlsHandshake = time.Now() },
+			TLSHandshakeDone: func(cs tls.ConnectionState, err error) {
+				logs["handshake"]=fmt.Sprintf("TLS Handshake: %v", time.Since(tlsHandshake).Seconds())
+			},
+
+			ConnectStart: func(network, addr string) { connect = time.Now() },
+			ConnectDone: func(network, addr string, err error) {
+				logs["connectTime"]=fmt.Sprintf("Connect time: %v", time.Since(connect).Seconds())
+			},
+			GotFirstResponseByte: func() {
+				logs["starttofirst"]=fmt.Sprintf("Time from start to first byte: %v", time.Since(start).Seconds())
+			},
+		}
+		proxyReq = proxyReq.WithContext(httptrace.WithClientTrace(req.Context(), trace))
+		proxyReq.Close = true
+
+		start = time.Now()
+		if resp, err := http.DefaultTransport.RoundTrip(proxyReq); err != nil {
+			return nil, err
+		} else {
+			var total = time.Since(start).Seconds()
+			if total>criticalResponseTime {
+				log.Println("[W] too long response: ",total, req.Host, req.URL)
+				for k, v:=range logs {
+					log.Println("[W]", req.Host, k,":",v)
+				}
+				log.Println("")
+			}
+			return resp, nil
+		}
+	}
+}
+
+
+func HttpProxyHandler(w http.ResponseWriter, req *http.Request, service *Service, query *string,
+	middlewares map[string]PluginInterface) error {
+	handler := GetDefaultHandler(service, query)
+	if middlewares != nil {
+		for _, filterName := range service.Plugins {
+			chainElement := handler
+			if plugin, ok := middlewares[filterName]; ok {
+				//lib.				pluginMethod = plugin.(func(*http.Request, func(*http.Request) (*http.Response, error)) (*http.Response, error))
+				handler = func(req *http.Request) (*http.Response, error) {
+					if _resp, err := plugin(req, chainElement); err!=nil {
+						return _resp, err
+					} else if _resp!=nil {
+						return _resp, err
+					} else {
+						return nil, errors.New(fmt.Sprint("got empty response from: ", req.URL))
+					}
+				}
+			} else {
+				log.Println(" cant load plugin ")
+			}
+		}
+	}
+
+	if req == nil {
+		return errors.New("nil request")
+	}
+
+	resp, err := handler(req)
+	if err != nil {
+		return err
+	}
+
+	if resp.Header != nil {
+		for responseHeader, responseHeaderValues := range resp.Header {
+			for _, responseHeaderValue := range responseHeaderValues {
+				w.Header().Add(responseHeader, responseHeaderValue)
+			}
+		}
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println( err.Error())
+		return err
+	} else {
+		defer  resp.Body.Close()
+		_, _ = w.Write(body)
+	}
+	return nil
+}
+
