f46b05ab5645caa56f88a68091bcaedc70488318
silago nevermind <silago.nevermind@gmail.com>
2019-06-04 10:39:50 +0300

    code refactoring

--- lib/proxy.go
+++ lib/proxy.go
@@ -1,12 +1,8 @@
 package lib
 
 import (
-	"errors"
-	//"fmt"
 	"github.com/gorilla/websocket"
-	"log"
 	"net/http"
-	"strings"
 )
 
 var upgrader = websocket.Upgrader{
@@ -17,32 +13,6 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
-// Find service matches by the url pattern
-// returns url, service, error
-func backend(c *Config, r *http.Request) (*Service, *string, error) {
-	var (
-		route string
-		query string
-	)
-
-	ps := strings.Split(r.URL.Path, "/")
-	// compare path with each rule
-	for rule, service := range c.Rules {
-		route = "/"
-		for index, part := range ps {
-			if part == "" {
-				continue
-			}
-			route += part + "/"
-
-			if strings.Index(route, rule) == 0 {
-				query = strings.Join(ps[index+1:], "/")
-				return &service, &query, nil
-			}
-		}
-	}
-	return nil, nil, errors.New("{\"error\":\"route not found\"}")
-}
 
 //func tryFallback(c *Config, r *http.Request) (string, string, bool) {
 //	if c.Version != "" && c.FallbackRule != "" {
@@ -51,25 +21,25 @@ func backend(c *Config, r *http.Request) (*Service, *string, error) {
 //	return "", "", false
 //}
 
-func New(c *Config, middlewares map[string]func(*http.Request, func(*http.Request) (*http.Response, error)) (*http.Response, error)) http.HandlerFunc {
-	return func(w http.ResponseWriter, req *http.Request) {
-		service, query, routeError := backend(c, req)
-		if routeError == nil {
-			switch proto := service.Protocol; proto {
-			case "ws":
-				if err := wsProxy(w, req, service, query, middlewares); err!=nil {
-                    log.Println("[error][websocket]:", err.Error())
-					_, _ = w.Write([]byte(err.Error()))
-				}
-			default:
-				if err := httpProxy(w, req, service, query, middlewares); err!=nil {
-					log.Println("[error][proxy]: ", err.Error(), service.Service, query)
-					_, _ = w.Write([]byte(err.Error()))
-				}
-			}
-		} else {
-			log.Println("route error", req.Host, req.URL, routeError.Error())
-			_, _ = w.Write([]byte("{\"error\":\"page not found\"}"))
-		}
-	}
-}
+//func New(c *Config, middlewares map[string]func(*http.Request, func(*http.Request) (*http.Response, error)) (*http.Response, error)) http.HandlerFunc {
+//	return func(w http.ResponseWriter, req *http.Request) {
+//		service, query, routeError := backend(c, req)
+//		if routeError == nil {
+//			switch proto := service.Protocol; proto {
+//			case "ws":
+//				if err := websocketProxyHandler(w, req, service, query, middlewares); err!=nil {
+//                    log.Println("[error][websocket]:", err.Error())
+//					_, _ = w.Write([]byte(err.Error()))
+//				}
+//			default:
+//				if err := httpProxy(w, req, service, query, middlewares); err!=nil {
+//					log.Println("[error][proxy]: ", err.Error(), service.Service, query)
+//					_, _ = w.Write([]byte(err.Error()))
+//				}
+//			}
+//		} else {
+//			log.Println("route error", req.Host, req.URL, routeError.Error())
+//			_, _ = w.Write([]byte("{\"error\":\"page not found\"}"))
+//		}
+//	}
+//}
--- main.go
+++ main.go
@@ -1,58 +1,22 @@
 package main
 
 import (
-	"errors"
-	"fmt"
+	//	"fmt"
 	"gateway/lib"
 	"log"
-	"net"
+	"syscall"
+
+	//	"net"
 	"net/http"
 	"os"
-	"os/signal"
-	"plugin"
-	"syscall"
 )
-
-func ENV(name string) string {
-	result := ""
-	if s, ok := os.LookupEnv(name); ok {
-		result = s
-	} else {
-		log.Fatal("Could not get env var " + name)
-	}
-	return result
-}
-
-type MiddlewarePlugin interface {
-	Init() func(*http.Request, func(*http.Request) (*http.Response, error)) (*http.Response, error)
-}
-
-func LoadPlugin(path string) (MiddlewarePlugin, error) {
-	mod, err := plugin.Open(path)
-	if err != nil {
-		return nil, err
-	}
-
-	plug, err := mod.Lookup("Plugin")
-	if err != nil {
-		return nil, err
-	}
-	plugin, ok := plug.(MiddlewarePlugin)
-	if !ok {
-		//fmt.Println("could not cast to Middleware plugin")
-		return nil, errors.New("could not cast to Middleware plugin")
-	}
-	return plugin, nil
-}
 
 func main() {
 	var (
 		configPath string
 	)
-	port := ENV("PORT")
+	port    := ENV("PORT")
 	sslPort := ENV("SSL_PORT")
-	middleware := make(map[string]func(*http.Request, func(*http.Request) (*http.Response, error)) (*http.Response, error))
-	//      func(*http.Request, func(*http.Request) (*http.Response, error)) (*http.Response, error))
 	if len(os.Args) != 2 {
 		if s, ok := os.LookupEnv("GATEWAY_CONFIG_FILE"); ok {
 			configPath = s
@@ -63,94 +27,25 @@ func main() {
 		configPath = os.Args[1]
 	}
 
-	config, err := lib.LoadConfig(configPath)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	sigChan :=make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGHUP)
+	//if config, err := lib.LoadConfig(configPath); err!=nil {
+	//	log.Fatal(err)
+	//} else {
 
-	go func(c *lib.Config) {
-		for sig:=range sigChan {
-			log.Printf("got {%s}  signal. reloading config ", sig)
-			if newConfig, err := lib.LoadConfig(configPath); err == nil {
-				*config=*newConfig
-			} else {
-				log.Printf(err.Error())
-				os.Exit(1)
-			}
-		}
-	}(config)
 
-	for pluginName, pluginPath := range config.Middleware {
-		plugin, err := LoadPlugin(pluginPath)
-		if err == nil {
-			middleware[pluginName] = plugin.Init()
-		} else {
-			fmt.Println("cant load plugin", pluginPath)
-			fmt.Println(err)
-		}
-	}
-
-	//log.Println("..")
-	for host, target := range config.PortForward {
-		listener, err := net.Listen("tcp", host)
-		defer listener.Close()
-		if err != nil {
-			log.Println(err.Error())
-		}
-
-		//log.Println("listening tcp at", host)
-		go func() {
-			for {
-				if conn, err := listener.Accept(); err!=nil {
-					log.Println(err.Error())
-					continue
-				} else if client, err := net.Dial("tcp", target); err!=nil {
-					conn.Close()
-					log.Println(err.Error())
-					continue
-				} else {
-					go func() {
-						for {
-							inputBuffer := make([]byte, 256)
-							if n, e := client.Read(inputBuffer); e != nil {
-								_ = conn.Close()
-								return
-							} else {
-								message := string(inputBuffer[:n])
-								_, _ = conn.Write([]byte(message))
-							}
-						}
-					}()
-
-					go func() {
-						for {
-							inputBuffer := make([]byte, 256)
-							if n, e := conn.Read(inputBuffer); e != nil {
-								_ = conn.Close()
-								return
-							} else {
-								message := string(inputBuffer[:n])
-								_, _ = client.Write([]byte(message))
-							}
-						}
-					}()
-				}
-			}
-		}()
-	}
-
+	gateway:=lib.InitGateway(configPath)
+	gateway.StartReloadOnSignal(syscall.SIGHUP)
+	http.HandleFunc("/", gateway.GetHandler())
+	//lib.InitGateway(configPath).Start()
 	http.HandleFunc("/google13dd0d8dae2fd927.html", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("google-site-verification: google13dd0d8dae2fd927.html"))
 	})
 
-	http.HandleFunc("/", lib.New(config, middleware))
 	log.Println("listen http at ", port)
 	go func() {
 		log.Fatal(http.ListenAndServeTLS(":"+sslPort, "server.crt", "server.key", nil))
 	}()
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 	select {}
+	//}
 }
